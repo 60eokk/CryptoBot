6822ff0c2e4bfe943d201c935c50fc74ebac32ba
Keith Jung <60eokk@gmail.com>
2023-06-23 22:21:39 -0500

    linechart now showing GRAPHS

--- svelte-app/src/routes/Detail.svelte
+++ svelte-app/src/routes/Detail.svelte
@@ -1,21 +1,17 @@
+<!-- Detail.svelte -->
 <script>
   import { onMount } from 'svelte';
   import { getContext } from 'svelte';
   import LineChart from './LineChart.svelte';
-  const { params } = getContext('routing');
 
-  let data = []; // This would hold the price history
-  let labels = []; // This will hold the labels for the chart
-
-  onMount(async () => {
-    // Fetch the price history using params.id
-    const response = await fetch(`/api/price_history/${params.id}`);
-    data = await response.json();
-
-    // Create labels for the data points
-    labels = data.map((_, index) => index);
-  });
+  let data = [];
+  let params = getContext('svelte-routing').params;
+  
+  $: {
+    fetch(`http://localhost:3000/prices/${params.pair}`)
+      .then(res => res.json())
+      .then(json => data = json);
+  }
 </script>
 
-<h1>{params.id}</h1>
-<LineChart {labels} {data} />
+<LineChart {data} />
--- svelte-app/src/routes/LineChart.svelte
+++ svelte-app/src/routes/LineChart.svelte
@@ -1,20 +1,48 @@
-
 <script>
-  import { onMount } from 'svelte';
-  import { Link } from 'svelte-routing';
+  import { onMount, onDestroy } from 'svelte';
+  import { Chart, LinearScale, TimeScale, LineController, PointElement, LineElement, Title } from 'chart.js';
+  import 'chartjs-adapter-date-fns';
 
-  let cryptocurrencies = [];
+  Chart.register(LinearScale, TimeScale, LineController, PointElement, LineElement, Title);
 
-  onMount(async () => {
-    const response = await fetch('http://localhost:3000/cryptocurrencies');
-    cryptocurrencies = await response.json();
+
+  export let data = [];
+  
+  let ctx;
+  let chart;
+
+  onMount(() => {
+    chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        datasets: [{
+          data: data,
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'day'
+            }
+          },
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  onDestroy(() => {
+    if (chart) {
+      chart.destroy();
+    }
   });
 </script>
 
-<h1>Trading</h1>
-
-<ul>
-  {#each cryptocurrencies as crypto}
-    <li><Link to={`/trading/${crypto}`}>{crypto}</Link></li>
-  {/each}
-</ul>
+<canvas bind:this={ctx}></canvas>
